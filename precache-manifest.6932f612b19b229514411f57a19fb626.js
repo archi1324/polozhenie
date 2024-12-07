750e07b425b6ebede0ed58876244d7c0192dc084
Артур Саяхов <artursaahov@MacBook-Air.local>
2024-12-07 20:25:08 +0500

    Updates

---
+++ precache-manifest.6932f612b19b229514411f57a19fb626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac811bbd5a77ddaea96b0cab6ca7664",
+    "url": "/polozhenie/index.html"
+  },
+  {
+    "revision": "327a91e74c32005ef400",
+    "url": "/polozhenie/static/css/main.6f04d774.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/polozhenie/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "09a517156b37d65a5968",
+    "url": "/polozhenie/static/js/3.0e9e86c2.chunk.js"
+  },
+  {
+    "revision": "327a91e74c32005ef400",
+    "url": "/polozhenie/static/js/main.90a620c9.chunk.js"
+  },
+  {
+    "revision": "7b549e6bcaa4b6abfa65",
+    "url": "/polozhenie/static/js/runtime~main.179b308e.js"
+  }
+]);
